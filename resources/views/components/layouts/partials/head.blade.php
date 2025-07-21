70b716d392f260b29c3851d225dc1e4d80d0bd39
David Molnar <david.molnar.mega@gmail.com>
2025-07-21 15:34:01 +0200

    add: title form config

--- resources/views/components/layouts/partials/head.blade.php
+++ resources/views/components/layouts/partials/head.blade.php
@@ -1,121 +1,61 @@
 <meta charset="utf-8">
 
-<meta
-    name="viewport"
-    content="width=device-width, initial-scale=1.0, viewport-fit=cover"
->
+<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
 
-<meta
-    name="csrf-token"
-    content="{{ csrf_token() }}"
->
+<meta name="csrf-token" content="{{ csrf_token() }}">
 
 <meta name="theme-color" />
 
 @guest
-    <meta
-        property="og:title"
-        content="Larasense - {{ $title ?? '' }}"
-    />
+    <meta property="og:title" content="Larasense - {{ $title ?? '' }}" />
 
-    <meta
-        property="og:description"
-        content="Stay updated on Laravel news, trends, and updates with curated content from top blogs, YouTube, and podcasts—all in a sleek, user-friendly design."
-    />
+    <meta property="og:description"
+        content="Stay updated on Laravel news, trends, and updates with curated content from top blogs, YouTube, and podcasts—all in a sleek, user-friendly design." />
 
-    <meta
-        name="description"
-        content="Stay updated on Laravel news, trends, and updates with curated content from top blogs, YouTube, and podcasts—all in a sleek, user-friendly design."
-    />
+    <meta name="description"
+        content="Stay updated on Laravel news, trends, and updates with curated content from top blogs, YouTube, and podcasts—all in a sleek, user-friendly design." />
 
-    <meta
-        property="og:type"
-        content="website"
-    />
+    <meta property="og:type" content="website" />
 
-    <meta
-        property="og:locale"
-        content="en"
-    />
+    <meta property="og:locale" content="en" />
 
-    <meta
-        property="og:image"
-        content="{{ asset('img/og_image.webp') }}"
-    />
+    <meta property="og:image" content="{{ asset('img/og_image.webp') }}" />
 
-    <meta
-        property="og:url"
-        content="{{ url()->current() }}"
-    />
+    <meta property="og:url" content="{{ url()->current() }}" />
 
-    <link
-        rel="canonical"
-        href="{{ url()->current() }}"
-    />
+    <link rel="canonical" href="{{ url()->current() }}" />
 
-    <meta
-        name="twitter:card"
-        content="summary_large_image"
-    >
+    <meta name="twitter:card" content="summary_large_image">
 
-    <meta
-        name="twitter:creator"
-        content="@nabilhassen08"
-    />
+    <meta name="twitter:creator" content="@nabilhassen08" />
 
-    <meta
-        name="twitter:title"
-        content="Larasense - {{ $title ?? '' }}"
-    />
+    <meta name="twitter:title" content="Larasense - {{ $title ?? '' }}" />
 
-    <meta
-        name="twitter:description"
-        content="Stay updated on Laravel news, trends, and updates with curated content from top blogs, YouTube, and podcasts—all in a sleek, user-friendly design."
-    />
+    <meta name="twitter:description"
+        content="Stay updated on Laravel news, trends, and updates with curated content from top blogs, YouTube, and podcasts—all in a sleek, user-friendly design." />
 
-    <meta
-        name="twitter:image"
-        content="{{ asset('img/og_image.webp') }}"
-    />
+    <meta name="twitter:image" content="{{ asset('img/og_image.webp') }}" />
 @endguest
 
-<title>Larasense - {{ $title ?? '' }}</title>
+<title>{{ config('app.name') }} - {{ $title ?? '' }}</title>
 
-<link
-    rel="icon"
-    type="image/png"
-    href="{{ asset('favicon.png') }}"
->
+<link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
 
-<link
-    rel="preload"
-    href="{{ asset('img/logo.png') }}"
-    as="image"
-    fetchpriority="high"
->
+<link rel="preload" href="{{ asset('img/logo.png') }}" as="image" fetchpriority="high">
 
 @if (request()->routeIs('home'))
-    <link
-        rel="preload"
-        href="{{ asset('img/light_screenshot.webp') }}"
-        as="image"
-        fetchpriority="high"
-    >
-    
-    <link
-        rel="preload"
-        href="{{ asset('img/dark_screenshot.webp') }}"
-        as="image"
-        fetchpriority="high"
-    >
+    <link rel="preload" href="{{ asset('img/light_screenshot.webp') }}" as="image" fetchpriority="high">
+
+    <link rel="preload" href="{{ asset('img/dark_screenshot.webp') }}" as="image" fetchpriority="high">
 @endif
 
 <script>
     function toggleDarkMode(params) {
-        const isThemeDark = localStorage.getItem('themeMode') === 'dark' || (!('themeMode' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches);
-        
+        const isThemeDark = localStorage.getItem('themeMode') === 'dark' || (!('themeMode' in localStorage) && window
+            .matchMedia('(prefers-color-scheme: dark)').matches);
+
         document.documentElement.classList.toggle('dark', isThemeDark);
-        
+
         document
             .querySelector('meta[name="theme-color"]')
             .setAttribute(
@@ -125,7 +65,7 @@
     }
 
     toggleDarkMode();
-    
+
     document.addEventListener('livewire:navigated', () => {
         toggleDarkMode();
     });
@@ -137,10 +77,7 @@
 
 @production
     <!-- Google tag (gtag.js) -->
-    <script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-XS5G6ZQC5P"
-    ></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-XS5G6ZQC5P"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
 
