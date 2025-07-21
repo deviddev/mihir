bee979083db223690a939ff14f5c3418c9ef3479
David Molnar <david.molnar.mega@gmail.com>
2025-07-21 14:50:01 +0200

    feat: remove scheduled queue

--- routes/console.php
+++ routes/console.php
@@ -6,6 +6,4 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('larasense:bot')->withoutOverlapping()->everyTenMinutes();
 
-Schedule::command('queue:work --tries=2 --stop-when-empty')->withoutOverlapping()->everySecond();
-
 Schedule::command('queue:prune-failed')->daily();
