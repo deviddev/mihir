06e7798aa5e9018a8e0258e2dccbf9140951bc6c
David Molnar <david.molnar.mega@gmail.com>
2025-07-21 14:56:15 +0200

    increase feed processing interval

--- routes/console.php
+++ routes/console.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('larasense:bot')->withoutOverlapping()->everyTenMinutes();
+Schedule::command('larasense:bot')->withoutOverlapping()->everyFiveMinutes();
 
 Schedule::command('queue:prune-failed')->daily();
