8425650630c2f55e7719ff334c9aac1b53845519
David Molnar <david.molnar.mega@gmail.com>
2025-07-22 15:10:37 +0200

    add privacy translation

--- resources/views/privacy-policy.blade.php
+++ resources/views/privacy-policy.blade.php
@@ -1,169 +1,168 @@
 <x-layouts.guest>
-    <x-slot:title>Privacy Policy</x-slot>
+    <x-slot:title>Adatkezelési tájékoztató</x-slot>
     <div class="space-y-20">
         <x-navbar />
         <div class="container mx-auto">
             <div class="sm:max-w-3xl sm:mx-auto mx-4 space-y-8">
                 <h1 class="font-semibold text-2xl">
-                    Privacy Policy
+                    Adatkezelési tájékoztató
                 </h1>
 
-                <h2 class="opacity-70">Last Updated: Novemeber 30, 2024</h2>
+                <h2 class="opacity-70">Utoljára frissítve: 2025. július 22.</h2>
 
                 <div>
-                    <h2 class="font-semibold text-lg mb-4"> Information We Collect </h2>
+                    <h2 class="font-semibold text-lg mb-4">Milyen adatokat gyűjtünk</h2>
                     <p class="opacity-70">
-                        Personal Information: When you sign up or log in to Larasense, we may collect personal
-                        information, including but not limited to:
+                        Személyes adatok: Amikor regisztrálsz vagy bejelentkezel a mihir.hu-ra, személyes adatokat
+                        gyűjthetünk, többek között:
                     </p>
                     <ul class="list-disc list-inside my-4 opacity-70">
-                        <li>Email address</li>
-                        <li>Name (optional)</li>
-                        <li>Password</li>
+                        <li>E-mail cím</li>
+                        <li>Név (opcionális)</li>
+                        <li>Jelszó</li>
                     </ul>
                     <p class="opacity-70">
-                        Non-Personal Information: We may also collect non-personal information through cookies and
-                        tracking technologies, including:
+                        Nem személyes adatok: Sütik és követési technológiák segítségével nem személyes adatokat is
+                        gyűjthetünk, például:
                     </p>
                     <ul class="list-disc list-inside mt-4 opacity-70">
-                        <li>IP address</li>
-                        <li>Browser type and version</li>
-                        <li>Pages viewed on the site</li>
-                        <li>Time spent on the site</li>
+                        <li>IP-cím</li>
+                        <li>Böngésző típusa és verziója</li>
+                        <li>Megtekintett oldalak</li>
+                        <li>Az oldalon eltöltött idő</li>
                     </ul>
                 </div>
 
                 <div>
-                    <h2 class="font-semibold text-lg mb-4"> How We Collect Your Information </h2>
+                    <h2 class="font-semibold text-lg mb-4">Hogyan gyűjtjük az adatokat</h2>
                     <ul class="list-disc list-inside mt-4 opacity-70">
-                        <li>Direct Collection: When you sign up for an account, subscribe to newsletters, or contact us,
-                            you provide personal information directly to us.</li>
-                        <li>Automated Collection: We collect non-personal information through the use of cookies,
-                            analytics tools, and other tracking technologies as you browse the site.</li>
+                        <li>Közvetlen adatgyűjtés: Amikor regisztrálsz, feliratkozol hírlevélre, vagy kapcsolatba lépsz
+                            velünk, közvetlenül adsz meg adatokat.</li>
+                        <li>Automatikus adatgyűjtés: Nem személyes adatokat sütik, elemző eszközök és egyéb követési
+                            technológiák segítségével gyűjtünk böngészés közben.</li>
                     </ul>
                 </div>
 
                 <div>
-                    <h2 class="font-semibold text-lg mb-4"> How We Use Your Information </h2>
+                    <h2 class="font-semibold text-lg mb-4">Hogyan használjuk az adatokat</h2>
                     <p class="opacity-70">
-                        We use the information we collect for the following purposes:
+                        Az általunk gyűjtött adatokat az alábbi célokra használjuk:
                     </p>
                     <ul class="list-disc list-inside mt-4 opacity-70">
-                        <li> To create and manage your account (sign-up, log-in). </li>
-                        <li> To send you newsletters and updates if you’ve subscribed. </li>
-                        <li> To improve the website, provide personalized content, and enhance your experience. </li>
-                        <li> To monitor site traffic and user behavior using tools like Google Analytics. </li>
-                    </ul>
-                </div>
-
-                <div>
-                    <h2 class="font-semibold text-lg mb-4"> Data Sharing and Third Parties </h2>
-                    <p class="opacity-70">
-                        We may share your information with third parties under the following circumstances:
-                    </p>
-                    <ul class="list-disc list-inside my-4 opacity-70">
-                        <li> Service Providers: We may share information with trusted third-party service providers who
-                            help us run the site (e.g., email service providers, analytics tools). </li>
-                        <li> Legal Requirements: We may disclose information if required by law or to protect our legal
-                            rights. </li>
-                        <li> Business Transfers: If we are involved in a merger, acquisition, or sale of assets, your
-                            information may be transferred as part of that transaction. </li>
-                    </ul>
-                    <p class="opacity-70">
-                        We do not sell or rent your personal information to third parties for marketing purposes.
-                    </p>
-                </div>
-
-                <div>
-                    <h2 class="font-semibold text-lg mb-4"> Cookies </h2>
-                    <p class="opacity-70">
-                        Larasense uses cookies to:
-                    </p>
-                    <ul class="list-disc list-inside my-4 opacity-70">
-                        <li>Improve your experience and enhance functionality.</li>
-                        <li>Track website usage for analytics purposes.</li>
-                    </ul>
-                    <p class="opacity-70">
-                        You can control cookie settings through your browser, but please note that disabling cookies may
-                        affect your ability to use certain features of the site.
-                    </p>
-                </div>
-
-                <div>
-                    <h2 class="font-semibold text-lg mb-4"> User Rights </h2>
-                    <p class="opacity-70">
-                        As a user, you have the following rights regarding your personal data:
-                    </p>
-                    <ul class="list-disc list-inside my-4 opacity-70">
-                        <li>Access: You can request to view the personal information we have collected about you.</li>
-                        <li>Correction: You can update or correct your information at any time through your account
-                            settings.</li>
-                        <li>Deletion: You may request to delete your account and personal information by contacting us.
+                        <li>Fiók létrehozása és kezelése (regisztráció, bejelentkezés)</li>
+                        <li>Hírlevelek és értesítések küldése, ha feliratkoztál</li>
+                        <li>A weboldal fejlesztése, személyre szabott tartalom nyújtása, felhasználói élmény javítása
                         </li>
-                        <li>Opt-Out: You can unsubscribe from our newsletters or marketing emails by following the
-                            instructions in the email.</li>
+                        <li>A weboldal forgalmának és felhasználói viselkedésének monitorozása (pl. Google Analytics
+                            segítségével)</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h2 class="font-semibold text-lg mb-4">Adatmegosztás és harmadik felek</h2>
+                    <p class="opacity-70">
+                        Az adataidat az alábbi esetekben oszthatjuk meg harmadik felekkel:
+                    </p>
+                    <ul class="list-disc list-inside my-4 opacity-70">
+                        <li>Szolgáltatók: Megbízható partnereinkkel (pl. e-mail szolgáltatók, analitikai eszközök) az
+                            oldal működtetése érdekében.</li>
+                        <li>Jogszabályi kötelezettségek: Amennyiben jogszabály előírja, vagy jogi jogaink védelme
+                            érdekében.</li>
+                        <li>Üzleti átruházások: egyesülés, felvásárlás esetén adataid átadásra kerülhetnek.</li>
                     </ul>
                     <p class="opacity-70">
-                        To exercise these rights, please contact us at <span
-                            class="underline font-bold">support@larasense.com</span>.
+                        Nem adjuk el és nem adjuk bérbe személyes adataidat harmadik feleknek marketing célokra.
                     </p>
                 </div>
 
                 <div>
-                    <h2 class="font-semibold text-lg mb-4"> Security </h2>
+                    <h2 class="font-semibold text-lg mb-4">Sütik (Cookies)</h2>
                     <p class="opacity-70">
-                        We implement reasonable security measures to protect your personal data. However, please note
-                        that no method of transmission over the internet is 100% secure, and we cannot guarantee the
-                        absolute security of your information.
+                        A mihir.hu sütiket használ a következő célokra:
+                    </p>
+                    <ul class="list-disc list-inside my-4 opacity-70">
+                        <li>A felhasználói élmény javítása és funkciók bővítése</li>
+                        <li>Weboldalhasználat követése statisztikai és elemzési célból</li>
+                    </ul>
+                    <p class="opacity-70">
+                        A böngésződ beállításaival kezelheted a sütiket, de kérjük, vedd figyelembe, hogy egyes funkciók
+                        nem működnek megfelelően sütik nélkül.
                     </p>
                 </div>
 
+                <div>
+                    <h2 class="font-semibold text-lg mb-4">Felhasználói jogok</h2>
+                    <p class="opacity-70">
+                        Felhasználóként az alábbi jogok illetnek meg a személyes adataiddal kapcsolatban:
+                    </p>
+                    <ul class="list-disc list-inside my-4 opacity-70">
+                        <li>Hozzáférés: Kérheted, hogy megtekinthesd a rólad tárolt személyes adatokat.</li>
+                        <li>Helyesbítés: Frissítheted vagy módosíthatod adataidat a fiókbeállításokban.</li>
+                        <li>Törlés: Kérheted a fiókod és adataid törlését kapcsolatfelvétel útján.</li>
+                        <li>Leiratkozás: Leiratkozhatsz hírleveleinkről az e-mailekben található utasítások követésével.
+                        </li>
+                    </ul>
+                    <p class="opacity-70">
+                        E jogaid gyakorlásához kérjük, vedd fel velünk a kapcsolatot: <span
+                            class="underline font-bold">support@mihir.hu</span>.
+                    </p>
+                </div>
 
                 <div>
-                    <h2 class="font-semibold text-lg mb-4"> Data Retention </h2>
+                    <h2 class="font-semibold text-lg mb-4">Adatbiztonság</h2>
                     <p class="opacity-70">
-                        We retain your personal data for as long as necessary to fulfill the purposes outlined in this
-                        Privacy Policy, or as required by law.
+                        Ésszerű biztonsági intézkedéseket alkalmazunk a személyes adatok védelme érdekében. Ugyanakkor
+                        kérjük, vedd figyelembe, hogy az internetes adatátvitel nem 100%-ban biztonságos, ezért az
+                        adatok teljes védelmét nem tudjuk garantálni.
+                    </p>
+                </div>
+
+                <div>
+                    <h2 class="font-semibold text-lg mb-4">Adatmegőrzés</h2>
+                    <p class="opacity-70">
+                        A személyes adatokat addig őrizzük meg, amíg az adatkezelési cél meg nem szűnik, vagy amíg a
+                        törvény előírja.
                     </p>
                     <ul class="list-disc list-inside mt-4 opacity-70">
                         <li>
-                            If you delete your account, we will retain your information for one week before it is
-                            permanently removed.
+                            Amennyiben törlöd a fiókodat, adataidat egy hétig őrizzük meg, mielőtt véglegesen törlésre
+                            kerülnek.
                         </li>
                     </ul>
                 </div>
 
                 <div>
-                    <h2 class="font-semibold text-lg mb-4"> Advertising and Third-Party Cookies </h2>
+                    <h2 class="font-semibold text-lg mb-4">Hirdetések és harmadik féltől származó sütik</h2>
                     <p class="opacity-70">
-                        We may use third-party advertising partners who use cookies to serve advertisements based on
-                        your visits to our site and other websites. You can control the use of cookies through your
-                        browser settings.
+                        Előfordulhat, hogy külső hirdetési partnereket használunk, akik sütik segítségével jelenítenek
+                        meg reklámokat az oldalunkon és más weboldalakon végzett látogatások alapján. A sütik
+                        használatát a böngésződ beállításaiban szabályozhatod.
                     </p>
                 </div>
 
                 <div>
-                    <h2 class="font-semibold text-lg mb-4"> Children’s Privacy </h2>
+                    <h2 class="font-semibold text-lg mb-4">Gyermekek adatainak védelme</h2>
                     <p class="opacity-70">
-                        Our website is not intended for users under the age of 13, and we do not knowingly collect
-                        personal information from children under 13. If you believe we have inadvertently collected such
-                        information, please contact us to have it removed.
+                        Weboldalunk nem ajánlott 18 éven aluliak számára, és nem gyűjtünk tudatosan adatokat ilyen korú
+                        gyermekektől. Ha úgy gondolod, hogy véletlenül gyűjtöttünk ilyen adatot, kérjük, vedd fel velünk
+                        a kapcsolatot, hogy törölhessük azt.
                     </p>
                 </div>
 
                 <div>
-                    <h2 class="font-semibold text-lg mb-4"> Changes to the Privacy Policy </h2>
+                    <h2 class="font-semibold text-lg mb-4">Az Adatkezelési Tájékoztató módosítása</h2>
                     <p class="opacity-70">
-                        We may update this Privacy Policy from time to time. Any changes will be posted on this page
-                        with an updated effective date. By continuing to use the site, you accept the revised policy.
+                        Időről időre módosíthatjuk az Adatkezelési Tájékoztatót. A változásokat ezen az oldalon tesszük
+                        közzé, frissített dátummal. A weboldal további használatával elfogadod a módosított
+                        szabályzatot.
                     </p>
                 </div>
 
                 <div>
-                    <h2 class="font-semibold text-lg mb-4"> Contact Us </h2>
+                    <h2 class="font-semibold text-lg mb-4">Kapcsolatfelvétel</h2>
                     <p class="opacity-70">
-                        If you have any questions or concerns about this Privacy Policy or how we handle your personal
-                        data, please contact us at <span class="underline font-bold">support@larasense.com</span>.
+                        Ha kérdésed vagy aggályod van az Adatkezelési Tájékoztatóval kapcsolatban, kérjük, írj nekünk a
+                        következő címen: <span class="underline font-bold">support@mihir.hu</span>.
                     </p>
                 </div>
             </div>
