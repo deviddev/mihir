1d89ef6ab1e3032cf00d00930324a5567345baa2
David Molnar <david.molnar.mega@gmail.com>
2025-07-22 20:13:54 +0200

    remove cache

--- resources/views/vendor/laravelpwa/offline.blade.php
+++ resources/views/vendor/laravelpwa/offline.blade.php
@@ -1,3 +1,13 @@
-<x-layouts.app>
+<html lang="hu">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ config('app.name') }} - {{ __('misc.offline') }}</title>
+</head>
+
+<body>
     <h1>{{ __('misc.you_are_currently_not_connected_to_any_networks') }}</h1>
-</x-layouts.app>
+</body>
+
+</html>
