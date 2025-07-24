1719ea9b2c4fb9095783935ecd769b0a7c4e8eaa
David Molnar <david.molnar.mega@gmail.com>
2025-07-24 16:20:56 +0200

    add: article count

--- resources/views/components/categories.blade.php
+++ resources/views/components/categories.blade.php
@@ -1,6 +1,7 @@
 <div class="flex flex-row flex-wrap gap-1.5 text-xs mb-4">
     @foreach ($categories as $category)
         <a href="{{ route('materials.index', ['category' => $category->slug]) }}"
-            class="bg-secondary dark:bg-secondary/50 text-white px-2 py-1 rounded">{{ $category->name }}</a>
+            class="bg-secondary dark:bg-secondary/50 text-white px-2 py-1 rounded">{{ $category->name }}
+            <small>({{ $category->materials_count }})</small></a>
     @endforeach
 </div>
